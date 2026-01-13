1cf81c1c089262aefb2cff7f79eaae07cd695e3b
Aman <109802995+amanadeem@users.noreply.github.com>
2026-01-13 23:30:02 +0500

    login form changes

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -98,7 +98,7 @@ export default function Login() {
           }}
         >
           <Typography variant="h4" fontWeight={700} textAlign="center" mb={1}>
-            Admin Login
+            Login Here
           </Typography>
 
           <Typography
